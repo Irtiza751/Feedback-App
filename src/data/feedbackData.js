63c2137858cd5d8ffa088cbda8196320704b8304
Muhammad Irtiza <91867702+Irtiza751@users.noreply.github.com>
2022-01-08 13:43:33 +0500

    Update feedbackData.js

--- src/data/feedbackData.js
+++ src/data/feedbackData.js
@@ -2,15 +2,20 @@ const feedbackData = [
   {
     id: 1,
     rating: 10,
-    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+    text: 'Hi Irtiza, My experience with this app was awesome, this is an awesome service thanks.',
   },
   {
     id: 2,
-    rating: 9,
+    rating: 10,
     text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
   },
   {
     id: 3,
+    rating: 9,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+  },
+  {
+    id: 4,
     rating: 8,
     text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
   },
